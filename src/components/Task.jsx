17d42857909577db0bd8d9ac5786a235c0f3096a
iamakitten1 <m.kostamashvili.1@gmail.com>
2024-06-25 19:39:51 +0200

    done

---
+++ src/components/Task.jsx
@@ -0,0 +1,13 @@
+import Checkbox from "./Checkbox";
+
+export default function Task({name,done}){
+    return(
+        <div className="task">
+            <Checkbox  defaultChecked={done}/>
+
+           {name}
+           
+        </div>
+    );
+}
+
